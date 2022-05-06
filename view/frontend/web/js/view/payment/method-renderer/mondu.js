6bf31fb51b833f467420b541dc1e52b1140253e5
Tigran <62335544+tikohov20@users.noreply.github.com>
2022-05-06 09:26:17 +0400

    onestepchecokut incorrect billingaddress and validation issues fix (#17)
    * onestepchecokut incorrect billingaddress and validation issues fix
    
    * fixed formatting

--- view/frontend/web/js/view/payment/method-renderer/mondu.js
+++ view/frontend/web/js/view/payment/method-renderer/mondu.js
@@ -1,134 +1,158 @@
+define([
+    "jquery",
+    "Magento_Checkout/js/model/quote",
+    "Magento_Checkout/js/view/payment/default",
+    "Magento_Checkout/js/action/redirect-on-success",
+    "Magento_Ui/js/model/messages",
+    "Magento_Checkout/js/model/payment/additional-validators",
+    "Magento_Checkout/js/view/billing-address",
+    "Magento_Checkout/js/action/set-payment-information",
+], function (
+  $,
+  quote,
+  Component,
+  redirectOnSuccessAction,
+  Messages,
+  additionalValidators,
+  billingAddress,
+  SetPaymentInformationAction
+) {
+    "use strict";
+    return Component.extend({
+        defaults: {
+            template: "Mondu_Mondu/payment/form",
+            transactionResult: "",
+            monduSkdLoaded: false,
+        },
+        isBillingSameAsShippng: true,
 
-define(
-    [
-        'jquery',
-        'Magento_Checkout/js/model/quote',
-        'Magento_Checkout/js/view/payment/default',
-        'Magento_Checkout/js/action/redirect-on-success',
-        'Magento_Ui/js/model/messages',
-    ],
-    function ($, quote, Component, redirectOnSuccessAction, Messages) {
-        'use strict';
-        return Component.extend({
-            defaults: {
-                template: 'Mondu_Mondu/payment/form',
-                transactionResult: '',
-                monduSkdLoaded: false
-            },
+        initObservable: function () {
+            self = this;
+            self._super().observe(["transactionResult"]);
+            billingAddress().isAddressSameAsShipping.subscribe(function (
+              isSame
+            ) {
+                self.isBillingSameAsShipping = isSame;
+            });
+            var monduSkd = document.createElement("script");
+            monduSkd.onload = function () {
+                self.monduSdkLoaded = true;
+            };
+            monduSkd.src = self.getMonduSdkUrl();
+            document.head.appendChild(monduSkd);
+            this.messageContainer = new Messages();
 
-            initObservable: function () {
-                self = this;
-                self._super()
-                    .observe([
-                        'transactionResult'
-                    ]);
+            return self;
+        },
 
-                var monduSkd = document.createElement('script');
-                monduSkd.onload = function() {
-                    self.monduSdkLoaded = true;
-                }
-                monduSkd.src= self.getMonduSdkUrl();
-                document.head.appendChild(monduSkd);
-                this.messageContainer = new Messages();
+        getCode: function () {
+            return "mondu";
+        },
 
-                return self;
-            },
+        getData: function () {
+            return {
+                method: this.item.method,
+                additional_data: {
+                    transaction_result: this.transactionResult(),
+                },
+            };
+        },
 
-            getCode: function () {
-                return 'mondu';
-            },
+        getTransactionResults: function () {
+            return _.map(
+              window.checkoutConfig.payment.mondu.transactionResults,
+              function (value, key) {
+                  return {
+                      value: key,
+                      transaction_result: value,
+                  };
+              }
+            );
+        },
 
-            getData: function () {
-                return {
-                    'method': this.item.method,
-                    'additional_data': {
-                        'transaction_result': this.transactionResult()
-                    }
-                };
-            },
+        getMonduCheckoutTokenUrl: function () {
+            return window.checkoutConfig.payment[self.getCode()]
+              .monduCheckoutTokenUrl;
+        },
 
-            getTransactionResults: function () {
-                return _.map(window.checkoutConfig.payment.mondu.transactionResults, function (value, key) {
-                    return {
-                        'value': key,
-                        'transaction_result': value
-                    }
-                });
-            },
+        getMonduSdkUrl: function () {
+            return window.checkoutConfig.payment[self.getCode()].sdkUrl;
+        },
 
-            getMonduCheckoutTokenUrl: function() {
-                return window.checkoutConfig.payment[self.getCode()].monduCheckoutTokenUrl;
-            },
+        getCustomerEmail: function () {
+            if (quote.guestEmail) {
+                return quote.guestEmail;
+            } else {
+                return customerData.email;
+            }
+        },
 
-            getMonduSdkUrl: function () {
-                return window.checkoutConfig.payment[self.getCode()].sdkUrl;
-            },
+        openCheckout: function (token) {
+            $(
+              '<div id="mondu-checkout-widget" style="position: fixed; top: 0;right: 0;left: 0;bottom: 0; z-index: 99999999;"/>'
+            ).appendTo("body");
+            window.monduCheckout.render({
+                token,
+                onCancel: () => {
+                    $("#mondu-checkout-widget").remove();
+                    self.isPlaceOrderActionAllowed(true);
+                    $("body").trigger("processStop");
+                },
+                onSuccess: () => {
+                    self.getPlaceOrderDeferredObject()
+                      .fail(function () {
+                          self.isPlaceOrderActionAllowed(true);
+                          $("body").trigger("processStop");
+                      })
+                      .done(function () {
+                          self.afterPlaceOrder();
+                          if (self.redirectAfterPlaceOrder) {
+                              redirectOnSuccessAction.execute();
+                          }
+                      });
+                    $("#mondu-checkout-widget").remove();
+                    $("body").trigger("processStop");
+                },
+                onClose: () => {},
+            });
+        },
 
-            getCustomerEmail: function() {
-                if (quote.guestEmail) {
-                    return quote.guestEmail;
-                } else {
-                    return customerData.email;
-                }
-            },
+        placeOrder: function (data, event) {
+            if (!additionalValidators.validate()) return;
+            if (event) {
+                event.preventDefault();
+            }
+            if (self.isBillingSameAsShippng) {
+                quote.billingAddress(quote.shippingAddress());
+            }
+            $("body").trigger("processStart");
 
-            openCheckout: function(token) {
-                $('<div id="mondu-checkout-widget" style="position: fixed; top: 0;right: 0;left: 0;bottom: 0; z-index: 99999999;"/>').appendTo('body');
-                window.monduCheckout.render({
-                    token,
-                    onCancel: () => {
-                        $('#mondu-checkout-widget').remove();
-                        self.isPlaceOrderActionAllowed(true);
-                        $('body').trigger('processStop');
-                    },
-                    onSuccess: () => {
-                        self.getPlaceOrderDeferredObject().fail(
-                            function() {
-                                self.isPlaceOrderActionAllowed(true);
-                                $('body').trigger('processStop');
-                            }
-                          ).done(
-                            function() {
-                                self.afterPlaceOrder();
-                                if (self.redirectAfterPlaceOrder) {
-                                    redirectOnSuccessAction.execute();
-                                }
-                            }
-                          );
-                          $('#mondu-checkout-widget').remove();
-                          $('body').trigger('processStop');
-                    },
-                    onClose: () => {
-                    }
-                });
-            },
-
-            placeOrder: function(data, event) {
-                if (event) {
-                    event.preventDefault();
-                }
-
-                $('body').trigger('processStart');
-
+            var initCheckout = function () {
                 $.ajax({
                     url: self.getMonduCheckoutTokenUrl(),
-                    method: 'get',
+                    method: "get",
                     data: {
-                        email: self.getCustomerEmail()
-                    }
-                }).always(function(res) {
-                    if(res && res.token && !res.error) {
+                        email: self.getCustomerEmail(),
+                    },
+                }).always(function (res) {
+                    if (res && res.token && !res.error) {
                         self.openCheckout(res.token);
                         return;
                     } else {
                         self.messageContainer.addErrorMessage({
-                            message: res.message
+                            message: res.message,
                         });
                     }
 
-                    $('body').trigger('processStop');
+                    $("body").trigger("processStop");
                 });
-            }
-        });
-    }
-);
+            };
+            SetPaymentInformationAction(
+              this.messageContainer,
+              self.getData()
+            ).then(() => {
+                initCheckout();
+            });
+        },
+    });
+});
