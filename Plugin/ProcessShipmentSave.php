9598d2d9812e0b15df59f0e22db2ee01fea9f3e7
ivan-pugach-mondu <155441636+ivan-pugach-mondu@users.noreply.github.com>
2024-02-10 19:51:04 +0200

    [PT-1020] Invoice Generation from API is not working (#98)

---
+++ Plugin/ProcessShipmentSave.php
@@ -0,0 +1,80 @@
+<?php
+namespace Mondu\Mondu\Plugin;
+
+use Magento\Framework\Exception\LocalizedException;
+use Mondu\Mondu\Helpers\InvoiceOrderHelper;
+use Mondu\Mondu\Helpers\Log;
+use Mondu\Mondu\Helpers\Logger\Logger as MonduFileLogger;
+
+class ProcessShipmentSave
+{
+    /**
+     * @var MonduFileLogger
+     */
+    private $monduFileLogger;
+
+    /**
+     * @var Log
+     */
+    protected $monduLogger;
+
+    /**
+     * @var InvoiceOrderHelper
+     */
+    private $invoiceOrderHelper;
+
+    /**
+     * @param MonduFileLogger $monduFileLogger
+     * @param Log $logger
+     * @param InvoiceOrderHelper $invoiceOrderHelper
+     */
+    public function __construct(
+        MonduFileLogger $monduFileLogger,
+        Log $logger,
+        InvoiceOrderHelper $invoiceOrderHelper
+    ) {
+        $this->monduFileLogger = $monduFileLogger;
+        $this->monduLogger = $logger;
+        $this->invoiceOrderHelper = $invoiceOrderHelper;
+    }
+
+    /**
+     * After Save Processing
+     *
+     * @param \Magento\Sales\Model\Order\ShipmentRepository\Interceptor $subject
+     * @param $shipment
+     *
+     * @return mixed
+     * @throws LocalizedException
+     */
+    public function afterSave(
+        \Magento\Sales\Model\Order\ShipmentRepository\Interceptor $subject,
+        $shipment
+    ) {
+        $order = $shipment->getOrder();
+        $monduLog = $this->monduLogger->getLogCollection($order->getData('mondu_reference_id'));
+
+        if ($monduLog->getSkipShipObserver()) {
+            $this->monduFileLogger
+                ->info(
+                    'Already invoiced using invoice orders action, skipping',
+                    ['orderNumber' => $order->getIncrementId()]
+                );
+
+            return $shipment;
+        }
+
+        $monduId = $order->getData('mondu_reference_id');
+        $this->monduLogger->syncOrder($monduId);
+
+        if (!$this->monduLogger->canShipOrder($monduId)) {
+            throw new LocalizedException(
+                __('Can\'t ship order: Mondu order state must be confirmed or partially_shipped')
+            );
+        }
+
+        $this->invoiceOrderHelper->handleInvoiceOrder($order, $shipment, $monduLog);
+
+        return $shipment;
+    }
+}
