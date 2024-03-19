9f85dbf02459e13737729b1bc9192c05d8d7c0fb
ivan-pugach-mondu <155441636+ivan-pugach-mondu@users.noreply.github.com>
2024-03-19 11:28:19 +0200

    [PT-1158] Magento2 plugin trying to confirm a declined order (#102)

--- Model/Request/ConfirmOrder.php
+++ Model/Request/ConfirmOrder.php
@@ -5,6 +5,7 @@ namespace Mondu\Mondu\Model\Request;
 use Magento\Framework\Exception\LocalizedException;
 use Magento\Framework\HTTP\Client\Curl;
 use Mondu\Mondu\Model\Ui\ConfigProvider;
+use Mondu\Mondu\Model\LogFactory;
 
 class ConfirmOrder extends CommonRequest implements RequestInterface
 {
@@ -19,15 +20,23 @@ class ConfirmOrder extends CommonRequest implements RequestInterface
     protected $configProvider;
 
     /**
+     * @var LogFactory
+     */
+    private $logger;
+
+    /**
      * @param Curl $curl
      * @param ConfigProvider $configProvider
+     * @param LogFactory $monduLogger
      */
     public function __construct(
         Curl $curl,
-        ConfigProvider $configProvider
+        ConfigProvider $configProvider,
+        LogFactory $monduLogger
     ) {
         $this->curl = $curl;
         $this->configProvider = $configProvider;
+        $this->logger = $monduLogger;
     }
 
     /**
@@ -35,6 +44,12 @@ class ConfirmOrder extends CommonRequest implements RequestInterface
      */
     protected function request($params)
     {
+        $log = $this->getLogData($params['orderUid']);
+
+        if ($log && $log['is_confirmed']) {
+            return true;
+        }
+
         $url = $this->configProvider->getApiUrl('orders') . '/' . $params['orderUid'] . '/confirm';
 
         $resultJson = $this->sendRequestWithParams(
@@ -54,5 +69,21 @@ class ConfirmOrder extends CommonRequest implements RequestInterface
         }
 
         return $result;
+    }
+
+    /**
+     * @param $orderUuid
+     *
+     * @return mixed
+     */
+    private function getLogData($orderUuid)
+    {
+        $monduLogger = $this->logger->create();
+
+        $logCollection = $monduLogger->getCollection()
+                                     ->addFieldToFilter('reference_id', ['eq' => $orderUuid])
+                                     ->load();
+
+        return $logCollection && $logCollection->getFirstItem()->getData() ? $logCollection->getFirstItem()->getData() : false;
     }
 }
