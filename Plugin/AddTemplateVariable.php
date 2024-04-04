cba8261bb386261a50d8e5bccb4585cfb09025dc
Ivan Pugach <ivan.pugach@ext.mondu.ai>
2024-04-04 16:35:07 +0300

    Extend invoice data

--- Plugin/AddTemplateVariable.php
+++ Plugin/AddTemplateVariable.php
@@ -2,12 +2,26 @@
 
 namespace Mondu\Mondu\Plugin;
 
+use Magento\Framework\App\Config\ScopeConfigInterface;
+use Magento\Store\Model\ScopeInterface;
 use Mondu\Mondu\Helpers\Log as MonduLogger;
 use Mondu\Mondu\Helpers\Logger\Logger as MonduFileLogger;
+use Mondu\Mondu\Model\Ui\ConfigProvider;
 
 class AddTemplateVariable
 {
-    public const VARIABLE_STRUCTURE = '<strong>%s:</strong> %s<br />';
+    public const MONDU_UK_SORT_CODE = '185008';
+    public const MONDU_EN_ACCOUNT_HOLDER = 'Mondu Capital S.à r.l.';
+    public const MONDU_FR_DE_ACCOUNT_HOLDER = 'Mondu Capital Sàrl';
+    public const MONDU_NL_ACCOUNT_HOLDER = 'Mondu Capital S.à r.l';
+    public const MONDU_EN_BANK_NAME = 'Citibank N.A.';
+    public const MONDU_EN_BIC = 'CITINL2X';
+    public const MONDU_DE_NL_BIC = 'HYVEDEMME40';
+    public const MONDU_FR_BIC = 'CITIFRPP';
+    public const UK_COUNTRY_CODE = 'UK';
+    public const DE_COUNTRY_CODE = 'DE';
+    public const FR_COUNTRY_CODE = 'FR';
+    public const NL_COUNTRY_CODE = 'NL';
 
     /**
      * @var MonduLogger
@@ -20,15 +34,31 @@ class AddTemplateVariable
     private MonduFileLogger $monduFileLogger;
 
     /**
-     * @param  MonduLogger      $monduLogger
-     * @param  MonduFileLogger  $monduFileLogger
+     * @var ConfigProvider
+     */
+    private ConfigProvider $config;
+
+    /**
+     * @var ScopeConfigInterface
+     */
+    private ScopeConfigInterface $scopeConfig;
+
+    /**
+     * @param  MonduLogger           $monduLogger
+     * @param  MonduFileLogger       $monduFileLogger
+     * @param  ConfigProvider        $config
+     * @param  ScopeConfigInterface  $scopeConfig
      */
     public function __construct(
         MonduLogger $monduLogger,
-        MonduFileLogger $monduFileLogger
+        MonduFileLogger $monduFileLogger,
+        ConfigProvider $config,
+        ScopeConfigInterface $scopeConfig
     ) {
         $this->monduLogger = $monduLogger;
         $this->monduFileLogger = $monduFileLogger;
+        $this->config = $config;
+        $this->scopeConfig = $scopeConfig;
     }
 
     /**
@@ -46,6 +76,7 @@ class AddTemplateVariable
         }
 
         try {
+            $vars['monduDetails'] = '';
             $monduReferenceId = $vars['order']->getMonduReferenceId();
             $monduLog = $this->monduLogger->getLogCollection($monduReferenceId);
 
@@ -53,35 +84,91 @@ class AddTemplateVariable
                 return [$vars];
             }
 
-            $vars['monduPaymentMethod'] = $vars['monduInvoiceIban'] = $vars['monduNetTerms'] = '';
+            $billingAddress = $vars['order']->getBillingAddress();
 
-            if ($monduLog['payment_method']) {
-                $vars['monduPaymentMethod'] = sprintf(
-                    self::VARIABLE_STRUCTURE,
-                    __('Payment Method'),
-                    ucwords(strtolower($monduLog['payment_method']))
-                );
-            }
-
-            if ($monduLog['invoice_iban']) {
-                $vars['monduInvoiceIban'] = sprintf(
-                    self::VARIABLE_STRUCTURE,
-                    __('IBAN'),
-                    ucwords(strtolower($monduLog['invoice_iban']))
-                );
-            }
-
-            if ($monduLog['authorized_net_term']) {
-                $vars['monduNetTerms'] = sprintf(
-                    self::VARIABLE_STRUCTURE,
-                    __('Payment term'),
-                    $monduLog['authorized_net_term'] . ' ' . __('days')
-                );
-            }
+            $vars['monduDetails'] = $this->getInvoiceDetails([
+                'countryId' => $billingAddress->getCountryId(),
+                'invoiceId' => $vars['invoice']->getIncrementId(),
+                'paymentMethod' => $vars['order']->getPayment()->getMethodInstance()->getTitle(),
+                'netTerms' => isset($monduLog['authorized_net_term']) ? $monduLog['authorized_net_term'] : ''
+            ]);
         } catch (\Exception $e) {
             $this->monduFileLogger->critical($e->getMessage());
         }
 
         return [$vars];
+    }
+
+    /**
+     * @param $invoiceDetails
+     *
+     * @return string
+     */
+    protected function getInvoiceDetails($invoiceData): string
+    {
+        switch ($invoiceData['paymentMethod']) {
+            case $this->scopeConfig->getValue('payment/mondu/title', ScopeInterface::SCOPE_STORE):
+                $invoiceDetails = $this->getPayLaterViaBankTransferDetails($invoiceData);
+                break;
+            case $this->scopeConfig->getValue('payment/mondusepa/title', ScopeInterface::SCOPE_STORE):
+                $invoiceDetails  = __('This invoice was created in accordance with the general terms and conditions of <strong>%1</strong> and <strong>Mondu GmbH</strong> for the purchase on account payment model.', $this->config->getMerchantTitle()) . '<br/>';
+                $invoiceDetails .= __('Since you have chosen the payment method to purchase on account with payment via SEPA direct debit through Mondu, the invoice amount will be debited from your bank account on the due date.') . '<br/>';
+                $invoiceDetails .= __('Before the amount is debited from your account, you will receive notice of the direct debit. Kindly make sure you have sufficient funds in your account.') . '<br/>';
+                break;
+            default:
+                $invoiceDetails  = __('This invoice was created in accordance with the general terms and conditions of <strong>%1</strong> and <strong>Mondu GmbH</strong> for the instalment payment model.', $this->config->getMerchantTitle()) . '<br/>';
+                $invoiceDetails .= __('Since you have chosen the instalment payment method via SEPA direct debit through Mondu, the individual installments will be debited from your bank account on the due date.') . '<br/>';
+                $invoiceDetails .= __('Before the amounts are debited from your account, you will receive notice regarding the direct debit. Kindly make sure you have sufficient funds in your account. In the event of changes to your order, the instalment plan will be adjusted to reflect the new order total.') . '<br/>';
+                break;
+        }
+
+        return $invoiceDetails;
+    }
+
+    /**
+     * @param $invoiceData
+     *
+     * @return string
+     */
+    protected function getPayLaterViaBankTransferDetails($invoiceData)
+    {
+        $invoiceDetails = __('This invoice is created in accordance with the terms and conditions of <strong>%1</strong> modified by <strong>Mondu GmbH</strong> payment terms. Please pay to the following account:', $this->config->getMerchantTitle()) . '<br/>';
+
+        switch ($invoiceData['countryId']) {
+            case self::UK_COUNTRY_CODE:
+                $invoiceDetails .= __('<strong>Account holder:</strong> %1', self::MONDU_EN_ACCOUNT_HOLDER) . '<br/>';
+                $invoiceDetails .= __('<strong>Bank:</strong> %1', self::MONDU_EN_BANK_NAME) . '<br/>';
+                $invoiceDetails .= __('<strong>Sort Code:</strong> %1', self::MONDU_UK_SORT_CODE) . '<br/>';
+                $invoiceDetails .= __('<strong>Account Number:</strong> %1', $this->config->getMerchantAccountNumber()) . '<br/>';
+                $invoiceDetails .= __('<strong>IBAN:</strong> %1', $this->config->getMerchantIban()) . '<br/>';
+                $invoiceDetails .= __('<strong>BIC:</strong> %1', self::MONDU_EN_BIC) . '<br/>';
+                break;
+            case self::DE_COUNTRY_CODE:
+                $invoiceDetails .= __('<strong>Account holder:</strong> %1', self::MONDU_FR_DE_ACCOUNT_HOLDER) . '<br/>';
+                $invoiceDetails .= __('<strong>IBAN:</strong> %1', $this->config->getMerchantIban()) . '<br/>';
+                $invoiceDetails .= __('<strong>BIC:</strong> %1', self::MONDU_DE_NL_BIC) . '<br/>';
+                break;
+            case self::FR_COUNTRY_CODE:
+                $invoiceDetails .= __('<strong>Account holder:</strong> %1', self::MONDU_FR_DE_ACCOUNT_HOLDER) . '<br/>';
+                $invoiceDetails .= __('<strong>IBAN:</strong> %1', $this->config->getMerchantIban()) . '<br/>';
+                $invoiceDetails .= __('<strong>BIC:</strong> %1', self::MONDU_FR_BIC) . '<br/>';
+                break;
+            case self::NL_COUNTRY_CODE:
+                $invoiceDetails .= __('<strong>Account holder:</strong> %1', self::MONDU_NL_ACCOUNT_HOLDER) . '<br/>';
+                $invoiceDetails .= __('<strong>IBAN:</strong> %1', $this->config->getMerchantIban()) . '<br/>';
+                $invoiceDetails .= __('<strong>BIC:</strong> %1', self::MONDU_DE_NL_BIC) . '<br/>';
+                break;
+            default:
+                $invoiceDetails .= __('<strong>Account holder:</strong> %1', self::MONDU_EN_ACCOUNT_HOLDER) . '<br/>';
+                $invoiceDetails .= __('<strong>Bank:</strong> %1', self::MONDU_EN_BANK_NAME) . '<br/>';
+                $invoiceDetails .= __('<strong>IBAN:</strong> %1', $this->config->getMerchantIban()) . '<br/>';
+                $invoiceDetails .= __('<strong>BIC:</strong> %1', self::MONDU_EN_BIC) . '<br/>';
+                break;
+        }
+
+        $invoiceDetails .= __('<strong>Payment reference:</strong> %1', $invoiceData['invoiceId']) . '<br/>';
+        $invoiceDetails .= __('<strong>Payment term:</strong> %1 days', $invoiceData['netTerms']) .  '<br/>';
+
+        return $invoiceDetails;
     }
 }
