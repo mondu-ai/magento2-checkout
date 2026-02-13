370c74df69043628ca0e13d205cbe5ca97b05289
Ivan Pugach <ivan.pugach@ext.mondu.ai>
2026-02-14 00:36:14 +0200

    Fix phpcs

--- Helpers/OrderHelper.php
+++ Helpers/OrderHelper.php
@@ -279,10 +279,8 @@ class OrderHelper
                 }
 
                 $orderItem = $i->getOrderItem();
-                if (
-                    $orderItem->getParentItem() &&
-                    $orderItem->getParentItem()->getProductType() === 'bundle'
-                ) {
+                if ($orderItem->getParentItem()
+                    && $orderItem->getParentItem()->getProductType() === 'bundle') {
                     continue;
                 }
 
