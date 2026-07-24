dd421d85dcce51f2cda8f09ac3e2077e4a4407c0
Ivan Pugach <ivan.pugach@ext.mondu.ai>
2026-07-24 13:19:56 +0300

    Implement improvements

--- Model/Payment/MonduCountryRestrictionTrait.php
+++ Model/Payment/MonduCountryRestrictionTrait.php
@@ -8,7 +8,15 @@ use Magento\Store\Model\ScopeInterface;
 
 trait MonduCountryRestrictionTrait
 {
+    private const CONFIG_PATH_ALLOW_SPECIFIC = 'payment/mondu/allowspecific';
+    private const CONFIG_PATH_SPECIFIC_COUNTRY = 'payment/mondu/specificcountry';
+
     /**
+     * $country is intentionally left untyped: Magento's MethodInterface /
+     * AbstractMethod::canUseForCountry($country) declares no parameter type, so
+     * adding one here would break signature compatibility in the payment
+     * methods that use this trait.
+     *
      * @param string $country
      * @return bool
      */
@@ -17,11 +25,11 @@ trait MonduCountryRestrictionTrait
         $storeId = $this->getStore();
 
         $allowSpecific = $this->_scopeConfig
-            ->getValue('payment/mondu/allowspecific', ScopeInterface::SCOPE_STORE, $storeId);
+            ->getValue(self::CONFIG_PATH_ALLOW_SPECIFIC, ScopeInterface::SCOPE_STORE, $storeId);
 
         if ($allowSpecific == 1) {
             $availableCountries = $this->_scopeConfig
-                ->getValue('payment/mondu/specificcountry', ScopeInterface::SCOPE_STORE, $storeId);
+                ->getValue(self::CONFIG_PATH_SPECIFIC_COUNTRY, ScopeInterface::SCOPE_STORE, $storeId);
             $availableCountries = explode(',', (string) $availableCountries);
             if (!in_array($country, $availableCountries, true)) {
                 return false;
